c56d040d7e281edc8a377ae106517ee927c7bba7
AlexD <adohrn@cs.odu.edu>
2016-01-12 14:18:40 -0500

    Initial Commit

---
+++ DestinyApiTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DestinyApiTest
+//
+//  Created by Alexander (Dohrn) Dohrn on 1/12/16.
+//  Copyright © 2016 Alexander (Dohrn) Dohrn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DestinyApiTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DestinyApiTest
+//
+//  Created by Alexander (Dohrn) Dohrn on 1/12/16.
+//  Copyright © 2016 Alexander (Dohrn) Dohrn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
